fdba8ed954362c32b2f3ebbb7d41df611ba2a360
Umar Khan <75561750+umrkhn@users.noreply.github.com>
2022-02-03 17:42:34 +0500

    fix: update validation in reviewSchema.js

--- server/validation/reviewSchema.js
+++ server/validation/reviewSchema.js
@@ -1,13 +1,13 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 const reviewSchema = Joi.object({
-    user: Joi.string().hex().required().min(24).max(24),
-    product: Joi.string().hex().required().min(24).max(24),
-    createdAt: Joi.date(),
-    rating: Joi.number().required().max(5),
-    description: Joi.string().alphanum().required().max(300),
-    firstName: Joi.string().pattern(new RegExp("^[a-zA-Z]{3,20}$")),
-    lastName: Joi.string().pattern(new RegExp("^[a-zA-Z]{3,20}$")),
+  user: Joi.string().hex().required().min(24).max(24),
+  product: Joi.string().hex().required().min(24).max(24),
+  createdAt: Joi.date(),
+  rating: Joi.number().required().max(5),
+  description: Joi.string().required().max(300),
+  firstName: Joi.string().pattern(new RegExp('^[a-zA-Z]{3,20}$')),
+  lastName: Joi.string().pattern(new RegExp('^[a-zA-Z]{3,20}$')),
 });
 
 export default reviewSchema;
